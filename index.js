bb4199042cb7399eeaf52f61bbf143fa1b7a0467
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-22 03:06:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 class Formatter {
   //add static methods here
   static capitalize(string){
-    return `${string.charAt(0).toUpperCase()} + ${string.split(1)}`
+    return string.charAt(0).toUpperCase() + string.slice(1)
   }
    static sanitize(string) {
-    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
-    return string.replace(/[^A-za-z0-9-']+/g, '')
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
    }
-   static titleize(string){
-     if(string !== 'the' || 'a' || 'an' || 'but' || 'of' || 'and' || 'for' || 'at' || 'by' || 'from'){
-       string.charAt(0).toUpperCase()
+  
+   static titleize(sentence){
+     let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ];
+     let result = [];
+     let words = sentence.split(" ");
+     for(let i = 0; i < words.length; i++){
+       if(i === 0) {
+         result.push(this.capitalize(words[i]))
+       } else {
+         if (exceptions.includes(words[i])){
+           result.push(words[i])
+         } else {
+           result.push(this.capitalize(words[i]))
+         }
        }
+     }
+     return result.join(" ")
    }
 }
